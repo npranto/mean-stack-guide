f11801ee5c92473a5b895ba3d34560150ec68e1b
Nazmuz Shakib Pranto <npranto@gmail.com>
2017-03-28 17:56:49 -0400

    trying to pass route params to display correct form

--- client/src/app/components/authenticate/authenticate.component.ts
+++ client/src/app/components/authenticate/authenticate.component.ts
@@ -7,7 +7,7 @@ declare const $: any;
   template: `
    
     <div class="page-header col-sm-12 col-md-6 col-md-offset-3 col-lg-4 col-lg-offset-4">
-      <h1 class="text-center"> Register <small></small></h1>
+      <h1 class="text-center">  <small></small></h1>
     </div>
     
     <div class="login-page col-sm-12 col-md-12 col-lg-12">
@@ -20,6 +20,10 @@ declare const $: any;
   styleUrls: ['./authenticate.component.css']
 })
 export class AuthenticateComponent implements OnInit {
+
+  authType: {
+
+  }
 
   constructor() { }
 
