3e565d6907083c2d46d116f6f0f9f3b4fea7295c
Nazmuz Shakib Pranto <npranto@gmail.com>
2017-03-29 12:53:57 -0400

    adds form titles by children route in authenticate component

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -6,12 +6,23 @@ declare const $: any;
   selector: 'msg-login',
   template: `
 
-      <form class="login-form">
-        <input type="text" placeholder="username"/>
-        <input type="password" placeholder="password"/>
-        <button>login</button>
-        <p class="message">Not registered? <a>Create an account</a></p>
-      </form>
+    <div class="page-header col-sm-12 col-md-6 col-md-offset-3 col-lg-4 col-lg-offset-4">
+      <h1 class="text-center"> Login <small></small></h1>
+    </div>
+    
+    <div class="login-page col-sm-12 col-md-12 col-lg-12">
+      <div class="form">
+        <form class="login-form">
+          <input type="text" placeholder="username"/>
+          <input type="password" placeholder="password"/>
+          <button>login</button>
+          <p class="message">Not registered? <a>Create an account</a></p>
+        </form>
+      </div>
+    </div>    
+
+
+      
      
   `,
   styleUrls: ['./login.component.css', './../authenticate/authenticate.component.css']
--- client/src/app/components/register/register.component.ts
+++ client/src/app/components/register/register.component.ts
@@ -5,13 +5,24 @@ declare const $: any;
 @Component({
   selector: 'msg-register',
   template: `
-      <form class="register-form">
-        <input type="text" placeholder="name"/>
-        <input type="password" placeholder="password"/>
-        <input type="text" placeholder="email address"/>
-        <button>create</button>
-        <p class="message">Already registered? <a>Sign In</a></p>
-      </form>
+
+    <div class="page-header col-sm-12 col-md-6 col-md-offset-3 col-lg-4 col-lg-offset-4">
+      <h1 class="text-center"> Register <small></small></h1>
+    </div>
+    
+    <div class="login-page col-sm-12 col-md-12 col-lg-12">
+      <div class="form">
+        <form class="register-form">
+          <input type="text" placeholder="name"/>
+          <input type="password" placeholder="password"/>
+          <input type="text" placeholder="email address"/>
+          <button>create</button>
+          <p class="message">Already registered? <a>Sign In</a></p>
+        </form>
+      </div>
+    </div>  
+
+      
   `,
   styleUrls: ['./register.component.css', './../authenticate/authenticate.component.css']
 })
